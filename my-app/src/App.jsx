2b13094c6d2294d0cb810e33a966fc2b82dfa9d4
Shahid-cs22 <mohamedshahid.cs22@stellamaryscoe.edu.in>
2025-09-25 21:46:54 +0530

    condition 2

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -4,18 +4,18 @@ import Navbar from './Components/Navbar'
 import Footer from './Components/Footer'
 import Page1 from './Components/page1'
 import Page2 from './Components/Page2'
-import Condition from './Components/Condition'
 import Condition2 from './Components/Condition2'
+import Condition1 from './Components/Condition1'
 const App = () => {
   return (
     <div>
 
       <Navbar/>
       <Page1/>
-      {/* <Condition/> */}
+      <Condition1/>
       <Condition2/>
       
-      {/* <Page2/> */}
+      <Page2/>
       <Footer/>
       
       
--- my-app/src/Components/Condition2.jsx
+++ my-app/src/Components/Condition2.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
 
-const Condition2 = (const islogin = true) => {
+const Condition2 = () => {
+
+    const login = true;
+
+    const fruit = ["mango", "banana", "grapes", "apple", "orange"];
+
     return (
+        
         <>
             <div className="conditiontwo-2">
 
                 <h1>{login ? "Welcome" : "Please Login"}</h1>
             </div>
+
+
+            <br />
+
+            <ul>
+                {fruit.map((item, index) => (<li key={item}>{item}</li>))}
+            </ul>
+
+
         </>
     )
 }

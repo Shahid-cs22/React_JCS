f949efe59f834d9f3f2698fdf08d914905eaca5b
Shahid-cs22 <mohamedshahid.cs22@stellamaryscoe.edu.in>
2025-10-23 17:57:58 +0530

    updated

--- task-vite/src/Display.jsx
+++ task-vite/src/Display.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Home from './Pages/Home'
+import Detail from './Pages/Cardss/Detail'
+
 
 
 const Display = () => {
@@ -9,6 +11,7 @@ const Display = () => {
 
 
                 <Home />
+                <Detail />
 
             </div>
         </>
--- task-vite/src/Pages/Home.jsx
+++ task-vite/src/Pages/Home.jsx
@@ -8,10 +8,10 @@ const Home = () => {
             <div>
                 <Container fluid className="">
 
-                    <Row className="justify-content-center align-items-center" style={{ minHeight: '100vh', backgroundImage: `url(${food})`, backgroundSize: 'cover', backgroundPosition: 'center', textShadow:"2px 2px 3px yellow", filter: "brightness(0.7)" }}>
-                        <Col lg={8} className="text-center text-white">
-                            <h1 className="display-4 fw-bold" style={{filter: "brightness(101%)"}}>Welcome to FoodHUB</h1>
-                            <p className="lead">Discover delicious recipes and culinary inspiration for every occasion.</p>
+                    <Row className="justify-content-center " style={{ minHeight: '100vh',alignContent:"center" , backgroundImage: `url(${food})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+                        <Col lg={8} className="text-center text-white justify-content-center ">
+                            <h1 className="display-4 fw-bold" >Welcome to <span style={{ color: "yellow" }}>FoodHUB</span> </h1>
+
                         </Col>
                     </Row>
                 </Container>

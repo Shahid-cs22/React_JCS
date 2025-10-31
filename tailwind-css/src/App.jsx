ae91bd7c555c1d503e4f266ea917a779c6254d8b
Shahid-cs22 <mohamedshahid.cs22@stellamaryscoe.edu.in>
2025-10-31 17:46:08 +0530

    tailwind css

---
+++ tailwind-css/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './App.css'
+
+const App = () => {
+
+
+  return (
+    <>
+      <div>
+        <h1 className='text-2xl text-red-400 font-bold underline'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum nam laboriosam quo!</h1>
+      </div> <br />
+      <hr />
+
+      <div className="mt-10 mx-10">
+        <h1 className='text-sm md:text-3xl text-blue-700 font-bold'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil
+          similique obcaecati iure animi rem vero numquam illum corrupti? Nesciunt, tempora!
+        </h1>
+
+        <p className="mt-4 text-yellow-300">
+
+          hello everyone
+        </p>
+
+      </div>
+    </>
+  )
+}
+
+export default App

1e419b00bb4e1d36185ed4189efe49ec5b8949d5
Shahid-cs22 <mohamedshahid.cs22@stellamaryscoe.edu.in>
2025-11-05 18:10:10 +0530

    updated

---
+++ sample-jsx/src/App.jsx
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import Navbar from './Components/Navbar'
+
+
+function App() {
+
+
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default App
---
+++ sample-jsx/src/Theme.js
@@ -0,0 +1,14 @@
+// import { createTheme } from "@mui/material";
+
+// export const theme = createTheme({
+//     palette: {
+
+//         primary: {
+//             main: '#ff6347'
+//         },
+//         secondary:{
+//             main:
+//         }
+//     }
+
+// })

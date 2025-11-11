7f8642c9f80cd5317e4e2d32df2d242edd9cbb3b
Shahid-cs22 <mohamedshahid.cs22@stellamaryscoe.edu.in>
2025-11-11 18:03:55 +0530

    updated

--- sample-jsx/src/App.jsx
+++ sample-jsx/src/App.jsx
@@ -1,19 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './App.css';
-import Navbar from './Components/Navbar';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from './Theme';
 import FirstPage from './pages/FirstPage';
 import SecondPage from './pages/SecondPage';
+import Nav2 from './Components/Nav2';
 
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Navbar />
+
+      <Nav2/>
       <FirstPage/>
       <SecondPage/>
     </ThemeProvider>
